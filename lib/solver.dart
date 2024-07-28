4f0f2c33563e7130d096f0b26a84bb42819da6e5
Raul Costa Junior <raulcostajunior@gmail.com>
2024-07-28 18:14:11 +0200

    Initial commit of Solver functionalities

--- lib/solver.dart
+++ lib/solver.dart
@@ -1,1 +1,51 @@
-class Solver {}
+import 'board.dart' show Board;
+
+/// Callback type for Solver to report its progress searching for solutions.
+typedef FindSolutionsProgress = void Function(
+    {int progress, int unsolvablesFound});
+
+class Solver {
+  /// Finds solution(s) for a given [puzzle] board.
+  ///
+  /// If defined, [progressCallback] will be called to report progress.
+  /// As [puzzle] is not guaranteed to be a "canonical" Sudoku board, which must
+  /// have only one solution, a [maxSolutions] maximum number of solutions to be
+  /// found can be specified.
+  ///
+  /// If [puzzle] is not a solvable board, an [ArgumentError] is thrown with
+  /// reason details. If
+  static List<Board> findSolutions(
+      final Board puzzle, final FindSolutionsProgress? progressCallback,
+      [final int maxSolutions = 1]) {
+    // Validate the parameters
+    if (maxSolutions < 1) {
+      throw ArgumentError('maxSolutions must be at least 1');
+    }
+    _checkSolvable(puzzle);
+
+    var solutions = <Board>[];
+    _findSolutions(puzzle, progressCallback, maxSolutions, solutions,
+        (level: 0, unsolvablesFound: 0, progress: 0));
+
+    return solutions;
+  }
+
+  static void _findSolutions(
+      final Board board,
+      final FindSolutionsProgress? progressCallback,
+      final int maxSolutions,
+      List<Board> solutions,
+      ({int level, int unsolvablesFound, int progress}) searchContext) {}
+
+  static void _checkSolvable(final Board board) {
+    if (!board.isValid) {
+      throw ArgumentError('The board is invalid');
+    }
+    if (board.isEmpty) {
+      throw ArgumentError('The board is empty');
+    }
+    if (board.isComplete) {
+      throw ArgumentError('The board has already been solved');
+    }
+  }
+}
