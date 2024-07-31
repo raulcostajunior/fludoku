1c22d79469328fc8b83cd008afebed99e8a3c7c8
Raul Costa Junior <raulcostajunior@gmail.com>
2024-07-31 10:47:11 +0200

    Fixed bug in find solutions progress report.

--- lib/solver.dart
+++ lib/solver.dart
@@ -2,7 +2,7 @@ import 'board.dart' show Board;
 
 /// Callback type for Solver to report its progress searching for solutions.
 /// [progress] is expected to be an integer between 0 and 100.
-typedef FindSolutionsProgress = void Function({int progress});
+typedef FindSolutionsProgress = void Function(int progress);
 
 class Solver {
   /// Finds solution(s) for a given [puzzle] board.
@@ -15,8 +15,8 @@ class Solver {
   /// If [puzzle] is not a solvable board, an [ArgumentError] is thrown. If
   /// [maxSolutions] is less than 1, an [ArgumentError] is thrown.
   static List<Board> findSolutions(final Board puzzle,
-      [final FindSolutionsProgress? progressCallback,
-      final int maxSolutions = 1]) {
+      {final FindSolutionsProgress? progressCallback,
+      final int maxSolutions = 1}) {
     // Validate the parameters
     if (maxSolutions < 1) {
       throw ArgumentError('maxSolutions must be at least 1');
@@ -25,7 +25,7 @@ class Solver {
 
     // Initialize progress reporting
     if (progressCallback != null) {
-      progressCallback(progress: 0);
+      progressCallback(0);
     }
 
     var solutions = <Board>[];
@@ -84,18 +84,20 @@ class Solver {
             col: blanks[possValsIdx].col,
             value: possVal);
         var newProgress = context.progress;
-        if (progressCallback != null) {
+        if (progressCallback != null && context.level == 0) {
           // Use the number of remaining blank positions as a rough progress
           // indicator; must also guarantee that the progress is monotonically
-          // ascending. This will be a good progress indicator only for "canonical"
-          // Boards with a single solution.
-          var currProgress = ((blanks.length - context.initialBlanks) /
+          // ascending. Only level 0 (searching within the original board
+          // puzzle) is considered for reporting progress. Otherwise, the
+          // progress would keep bouncing due to the required backtracks that
+          // happen at deeper levels.
+          var currProgress = ((context.initialBlanks - blanks.length) /
                   context.initialBlanks *
                   100)
               .toInt();
           if (currProgress > context.progress) {
             newProgress = currProgress;
-            progressCallback(progress: newProgress);
+            progressCallback(newProgress);
           }
         }
         _findSolutions(nextBoard, progressCallback, maxSolutions, solutions, (
@@ -113,10 +115,10 @@ class Solver {
         // The board is not solvable
         throw ArgumentError("Board is not solvable");
       }
-      // Or all the solutions have been found and they are not in a number
-      // greater than maxSolutions.
+      // Or all the solutions (or maximum number of allowed solutions) have been
+      // found.
       if (progressCallback != null) {
-        progressCallback(progress: 100);
+        progressCallback(100);
       }
     }
   }
