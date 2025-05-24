f89addb404cc5bfc39f561a45844dfe4dbdf3d5d
Raul Costa Junior <raulcostajunior@gmail.com>
2025-05-24 17:23:17 +0200

    Add TimeoutTracker class

---
+++ lib/src/timeout_tracker.dart
@@ -0,0 +1,20 @@
+class TimeoutTracker {
+  final int _timeoutMillis;
+  late int _startTimeMillis;
+
+  TimeoutTracker(this._timeoutMillis, {int? startTimeMillis}) {
+    if (startTimeMillis != null) {
+      _startTimeMillis = startTimeMillis;
+    } else {
+      _startTimeMillis = DateTime.now().millisecondsSinceEpoch;
+    }
+  }
+
+  bool get timedout =>
+      DateTime.now().millisecondsSinceEpoch - _startTimeMillis >=
+      _timeoutMillis;
+
+  int get remainingMillis =>
+      _timeoutMillis -
+      (DateTime.now().millisecondsSinceEpoch - _startTimeMillis);
+}
